9901ded88e6c1b563e95fc2ff3bd2946e1ea7565
mrlutik <yaievgeniy@gmail.com>
2023-06-12 12:34:59 +0200

    Add features
    Docker installatin/check, signature check, package install/check

--- internal/cli/deploy/deploy.go
+++ internal/cli/deploy/deploy.go
@@ -90,33 +90,7 @@ func createSSHClient(host string, privKeyPath string) (*ssh.Client, error) {
 
 	return client, nil
 }
-func installKeys(client *ssh.Client, privKey, pubKey string) error {
-	session, err := client.NewSession()
-	if err != nil {
-		return fmt.Errorf("Failed to create session: %v", err)
-	}
-	defer session.Close()
 
-	// Create .ssh directory if it doesn't exist
-	_, err = session.Output("mkdir -p ~/.ssh")
-	if err != nil {
-		return fmt.Errorf("Failed to create .ssh directory: %v", err)
-	}
-
-	// Write the public key to authorized_keys
-	_, err = session.Output(fmt.Sprintf("echo '%s' >> ~/.ssh/authorized_keys", pubKey))
-	if err != nil {
-		return fmt.Errorf("Failed to add public key to authorized_keys: %v", err)
-	}
-
-	// Set permissions for the .ssh directory and authorized_keys file
-	_, err = session.Output("chmod 700 ~/.ssh; chmod 600 ~/.ssh/authorized_keys")
-	if err != nil {
-		return fmt.Errorf("Failed to set permissions: %v", err)
-	}
-
-	return nil
-}
 func forbidRootLogin(client *ssh.Client) error {
 	session, err := client.NewSession()
 	if err != nil {
